affa9cdf010da21b846cef16730eab217adf0346
Shubhamkumar509 <shubhamkumar8210092530@gmail.com>
2024-09-27 14:43:44 +0530

    uplo1ad

---
+++ physics_08.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Load the dataset
+Replace 'Physics.csv' with the actual path to your dataset
+data = pd.read_csv('Physics.csv')
+print(data.head(100))  # Print the entire DataFrame
+
